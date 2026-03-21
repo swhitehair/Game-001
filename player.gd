452e2d221c8a765caaf8796cf11a2d00d53ccc43
swhitehair <swhitehair006@gmail.com>
2026-03-21 14:07:15 -0400

    animated my character
    when moving he is animated

--- player.gd
+++ player.gd
@@ -1,16 +1,25 @@
 extends CharacterBody2D
 
 @export var speed :int = 400
+var play_animation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func get_input():
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
+
+
+func get_input():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	if direction:
+		$AnimatedSprite2D.play("walking")
+	else:
+		$AnimatedSprite2D.stop()
+	
+func animated_walk():
+	pass
