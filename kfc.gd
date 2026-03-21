339e6694ec9b2e35bada8166530d3379896c1d01
swhitehair <swhitehair006@gmail.com>
2026-03-21 13:49:11 -0400

    can now successfully enter the KFC
    now i can go inside kfc

--- kfc.gd
+++ kfc.gd
@@ -1,10 +1,13 @@
 extends StaticBody2D
 var enterable = false
+var new_scene = preload("res://kfc_inside.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func go_to_kfc():
+	get_tree().change_scene_to_packed(new_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,3 +21,4 @@ func enter_door():
 	if enterable:
 		if Input.is_action_just_pressed("enter"):
 			print("success")
+			go_to_kfc()
